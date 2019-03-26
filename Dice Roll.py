28b7d30f52f1ae145dbe104d4c320bb67ceb599e
jacobjerome1993@gmail.com <jacobjerome1993@gmail.com>
2019-03-26 16:56:36 -0400

    I made changes to the program so it can loop over an incorrect answer that isn't 'yes' or 'no' when it ask the player to roll dice.

--- Dice Roll.py
+++ Dice Roll.py
@@ -2,20 +2,26 @@
 import random
 #dice roll function
 def dice_roll():
-	print("-Dice Roll Simulator-")
-	print("-Answer yes or no-")
-	#the "q" variable uses an input function to ask player to roll dice.
-	q = input("Would you like to roll the dice? ")
-	# Use a while loop to ask player if they would like to play again and repeat the process
-	while True:
-		dice = random.randint(1,6)
-		if q == "yes":
-			print("You rolled a "+str(dice)+".")
-			replay = input("Would you like to roll again? ")
-			if replay == "no":
-				break
-		elif q == "no":
-			break
+    print("-Dice Roll Simulator-")
+    print("-Answer yes or no-")
+    q = ""
+    #the "q" variable uses an input function to ask player to roll dice.
+    while q != "yes" or q != "no":
+        q = input("Would you like to roll the dice? ")
+        if q == "yes" or q == "no":
+            break
+    # Use a while loop to ask player if they would like to play again and repeat the process
+    while True:
+        dice = random.randint(1,6)
+        if q == "yes":
+            print("You rolled a "+str(dice)+".")
+            replay = input("Would you like to roll again? ")
+            if replay == "no":
+                print("Goodbye!")
+                break
+        elif q == "no":
+            print("See you again real soon!")
+            break
 
 
 #Calling the dice_roll() function
